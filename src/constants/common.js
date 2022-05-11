6d1cbbbf678ba88be31ef546d987c3efc57abd0a
[BK]Phù Dung <thuongtt@edsolabs.com>
2022-05-11 13:39:39 +0700

    VR-1620

--- src/constants/common.js
+++ src/constants/common.js
@@ -5,7 +5,8 @@ export const API_URL = process.env.REACT_APP_BASE_URL || 'http://10.0.0.62:10001
 export const STORAGE = {
   TOKEN: 'token',
   USER: 'user',
-  PERMISSIONS: 'permissions'
+  USER_PERMISSIONS: 'user_permissions',
+  REFRESH_TOKEN: 'refreshToken',
 };
 
 // export const IS_SERVER = typeof window === 'undefined';
