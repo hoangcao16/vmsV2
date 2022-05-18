57738f5ac34898d90cd3eb24a24ec0077b00c6c3
Tuan Ha <tuanhq@edsolabs.com>
2022-05-18 01:51:55 +0000

    [VR-1635][VMS 2.0][UI&UX] Tìm kiếm danh sách file capture theo bộ lọc nâng cao

--- src/services/dailyArchive/DailyArchiveApi.js
+++ src/services/dailyArchive/DailyArchiveApi.js
@@ -4,6 +4,14 @@ const DailyArchiveApi = {
   getAllDailyArchive: (params) => {
     return request.get(`/cctv-controller-svc/api/v1/files`, { params: params });
   },
+
+  getAllGroupCamera: (params) => {
+    return request.get(`/cctv-controller-svc/api/v1/camera_groups`, { params: params });
+  },
+
+  getAllCamera: (params) => {
+    return request.get(`/cctv-controller-svc/api/v1/cameras`, { params: params });
+  },
 };
 
 export default DailyArchiveApi;
