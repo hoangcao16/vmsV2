574d61565da82f874ca013ead3c35ce3a33b1ca6
TuanHQ <tuanhq@edsolabs.com>
2022-05-26 03:35:26 +0000

    [VR-1822] [VMS 2.0][UI&UX] Tách components trong Drawer

--- src/pages/storage/constants.js
+++ src/pages/storage/constants.js
@@ -3,3 +3,6 @@ export const CAPTURED_NAMESPACE = 'captured';
 export const EVENT_FILES_NAMESPACE = 'eventFiles';
 export const IMPORTANT_NAMESPACE = 'important';
 export const EVENT_AI_NAMESPACE = 'eventAI';
+
+export const LIST_VIEW = 'list';
+export const GRID_VIEW = 'grid';
--- src/pages/storage/index.jsx
+++ src/pages/storage/index.jsx
@@ -2,11 +2,8 @@ import { PageContainer } from '@ant-design/pro-layout';
 import { Tabs } from 'antd';
 import React, { useEffect, useState } from 'react';
 import { connect, useIntl } from 'umi';
-import Captured from './components/Captured';
-import TableDailyArchive from './components/DaiyArchive/TableDailyArchive';
-import EventFiles from './components/EventFiles';
-import EventAI from './components/EventAI';
 import ExtraFilter from './components/extraFilter';
+import TableStorage from './components/tableStorage';
 import {
   CAPTURED_NAMESPACE,
   DAILY_ARCHIVE_NAMESPACE,
@@ -65,47 +62,34 @@ function Storage({ dispatch }) {
             id: 'view.storage.daily_archive_files_list',
           })}`}
           key={DAILY_ARCHIVE_NAMESPACE}
-        >
-          <ExtraFilter nameSpace={DAILY_ARCHIVE_NAMESPACE} />
-          <TableDailyArchive />
-        </TabPane>
+        />
         <TabPane
           tab={`${intl.formatMessage({
             id: 'view.storage.captured_files_list',
           })}`}
           key={CAPTURED_NAMESPACE}
-        >
-          <ExtraFilter nameSpace={CAPTURED_NAMESPACE} />
-          <Captured />
-        </TabPane>
+        />
         <TabPane
           tab={`${intl.formatMessage({
             id: 'view.storage.event_files_list',
           })}`}
           key={EVENT_FILES_NAMESPACE}
-        >
-          <ExtraFilter nameSpace={EVENT_FILES_NAMESPACE} />
-          <EventFiles />
-        </TabPane>
+        />
         <TabPane
           tab={`${intl.formatMessage({
             id: 'view.storage.important_files_list',
           })}`}
           key={IMPORTANT_NAMESPACE}
-        >
-          <ExtraFilter nameSpace={IMPORTANT_NAMESPACE} />
-          tab4
-        </TabPane>
+        />
         <TabPane
           tab={`${intl.formatMessage({
             id: 'view.storage.ai_files_list',
           })}`}
           key={EVENT_AI_NAMESPACE}
-        >
-          <ExtraFilter nameSpace={EVENT_AI_NAMESPACE} />
-          <EventAI />
-        </TabPane>
+        />
       </Tabs>
+      <ExtraFilter nameSpace={nameSpace} />
+      <TableStorage nameSpace={nameSpace} />
     </PageContainer>
   );
 }
