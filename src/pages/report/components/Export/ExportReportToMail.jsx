4237b70e54eb502244a9f9cd7d9e95fbd8fda5e4
hainl <hainl@edsolabs.com>
2022-05-26 14:05:33 +0700

    VR-1606

--- src/pages/report/components/Export/ExportReportToMail.jsx
+++ src/pages/report/components/Export/ExportReportToMail.jsx
@@ -2,7 +2,7 @@ import { SendOutlined } from '@ant-design/icons';
 import { connect } from 'dva';
 import React from 'react';
 
-const ExportReportToMail = () => {
+const ExportReportToMail = (props) => {
   return <SendOutlined />;
 };
 
