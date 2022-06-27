1d0469551dd91c3c5a55e7c50750371ddc5444a4
tuandv21 <tuandv@edsolabs.com>
2022-06-27 15:11:52 +0700

    Bug biểu tượng loading nên đặt vào vị trí giữa màn hình

--- config/config.js
+++ config/config.js
@@ -8,62 +8,61 @@ import routes from './routes';
 const { REACT_APP_ENV } = process.env;
 
 export default defineConfig({
-  hash: true,
-  antd: {},
-  dva: {
-    hmr: true,
-  },
-  layout: {
-    locale: true,
-    siderWidth: 208,
-    ...defaultSettings,
-  },
-  locale: {
-    default: 'vi-VN',
-    antd: true,
-    baseNavigator: true,
-  },
-  dynamicImport: {
-    loading: '@ant-design/pro-layout/es/PageLoading',
-  },
-  targets: {
-    ie: 11,
-  },
-  routes: routes,
-  access: {},
-
-  theme: {
-    primaryColor: '#1890ff',
-  },
-
-  esbuild: {},
-  title: false,
-  ignoreMomentLocale: true,
-  proxy: proxy[REACT_APP_ENV || 'dev'],
-  manifest: {
-    basePath: '/',
-  },
-
-  fastRefresh: {},
-  openAPI: [
-    {
-      requestLibPath: "import { request } from 'umi'",
-      schemaPath: join(__dirname, 'oneapi.json'),
-      mock: false,
+    hash: true,
+    antd: {},
+    dva: {
+        hmr: true,
     },
-    {
-      requestLibPath: "import { request } from 'umi'",
-      schemaPath: 'https://gw.alipayobjects.com/os/antfincdn/CA1dOm%2631B/openapi.json',
-      projectName: 'swagger',
+    layout: {
+        locale: true,
+        siderWidth: 208,
+        ...defaultSettings,
     },
-  ],
-  nodeModulesTransform: {
-    type: 'none',
-  },
-  // mfsu: {},
-  webpack5: {},
-  exportStatic: {},
-  define: {
-    ...process.env,
-  },
+    locale: {
+        default: 'vi-VN',
+        antd: true,
+        baseNavigator: true,
+    },
+    dynamicImport: {
+        loading: '@/components/CustomLoading',
+    },
+    targets: {
+        ie: 11,
+    },
+    routes: routes,
+    access: {},
+
+    theme: {
+        primaryColor: '#1890ff',
+    },
+
+    esbuild: {},
+    title: false,
+    ignoreMomentLocale: true,
+    proxy: proxy[REACT_APP_ENV || 'dev'],
+    manifest: {
+        basePath: '/',
+    },
+
+    fastRefresh: {},
+    openAPI: [{
+            requestLibPath: "import { request } from 'umi'",
+            schemaPath: join(__dirname, 'oneapi.json'),
+            mock: false,
+        },
+        {
+            requestLibPath: "import { request } from 'umi'",
+            schemaPath: 'https://gw.alipayobjects.com/os/antfincdn/CA1dOm%2631B/openapi.json',
+            projectName: 'swagger',
+        },
+    ],
+    nodeModulesTransform: {
+        type: 'none',
+    },
+    // mfsu: {},
+    webpack5: {},
+    exportStatic: {},
+    define: {
+        ...process.env,
+    },
 });
